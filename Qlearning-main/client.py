0afe5397b58c86d4b2d4d2cd77a28a17ce0e8619
João Vítor Régis <jvcr@cin.ufpe.br>
2023-12-06 09:35:54 -0300

    feat: q-learning initial commit

--- Qlearning-main/client.py
+++ Qlearning-main/client.py
@@ -1,1 +1,7 @@
 #Aqui vocês irão colocar seu algoritmo de aprendizado
+import connection as cn
+
+socket = cn.connect(2037)
+
+while True:
+    estado, recompensa = cn.get_state_reward(socket, "jump")
