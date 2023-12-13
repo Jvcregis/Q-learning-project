059bd1bad18f0eb55d8ad719791d362de28c7001
Joao Vitor Cavalcanti Regis <jvcr@WINDOWS.CIN.UFPE.BR>
2023-12-13 09:02:58 -0300

    feat: choose action Add choose action function

--- Qlearning-main/client.py
+++ Qlearning-main/client.py
@@ -1,7 +1,23 @@
 #Aqui vocês irão colocar seu algoritmo de aprendizado
 import connection as cn
+import numpy as np
 
 socket = cn.connect(2037)
 
+Q_table = []
+
+def choose_action(Q_table, current_state, eps):
+    # Exploration: escolhe ação aleatoriamente
+    if np.random.rand() < eps: # Gera um número aleatório entre 0 e 1 e verifica se é menor que epsilon
+        action = np.random.choice(len(Q_table[current_state]))
+    # Exploration: escolhe a ação com o maior valor conhecido da Q_table
+    else:
+        action = np.argmax(Q_table[current_state])
+    return action
+
+
 while True:
+    # Ação Inicial
+    estado_inicial = "0b0000000"
+    #while estado != estado_terminal
     estado, recompensa = cn.get_state_reward(socket, "jump")
